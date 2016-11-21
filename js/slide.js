2796fe6a40c558b1cc6b19b6b831759ec4f5da02
yangholmes <yangholmes@126.com>
2016-11-21 21:28:44 +0800

    finish simple drag~

--- js/slide.js
+++ js/slide.js
@@ -1,8 +1,28 @@
-var panels = document.getElementsByClassName('panel');
+var slide = document.getElementsByClassName('slide')[0],
+    panels = document.getElementsByClassName('panel'),
+    panel = {},
+    leftButtonDown = false,
+    startY = 0, endY = 0,
+    originTop = 0,
+    movementY = 0
+    ;
 
 for(var i=0;i<panels.length;i++){
-  console.log(panels[i]);
-  panels[i].onmousedown = function(e){
-    console.log(e);
+  panel = panels[i];
+  panel.onmousedown = function(e){
+    leftButtonDown = true;
+    startY = e.clientY;
+    originTop = Number(slide.style.top.slice(0, -2));
+}
+  panel.onmouseup = function(e){
+    leftButtonDown = false;
+  }
+  panel.onmousemove = function(e){
+    if(leftButtonDown){
+      endY = e.clientY;
+      movementY = endY - startY;
+      var newTop = originTop + movementY;
+      slide.style.top = newTop + 'px';
+    }
   }
 };
